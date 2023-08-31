2bceb9605a85427f9d318f7748b26cf81a6e4a4f
jeongjiwon <jeewonre@hufs.ac.kr>
2023-08-31 17:48:48 +0900

    맑은 날씨 도시별 사진 조건 추가

--- src/controller/weather.controller.js
+++ src/controller/weather.controller.js
@@ -21,21 +21,29 @@ exports.getWeatherByCityName = async (req, res, next) => {
   }
   const weatherAPI = process.env.WEATHER_API_KEY;
   const cityCoordinates = cityNameCoordinates[cityName];
+  console.time("weather API call");
   const weatherDate = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cityCoordinates.lat}&lon=${cityCoordinates.lng}&appid=${weatherAPI}`);
   const weatherName = weatherDate.data.weather[0].main;
+  console.timeEnd("weather API call");
   const resImage = { image: null };
   if (["Mist", "Rain"].indexOf(weatherName) !== -1) {
     resImage.image = weatherImage.rain;
   }
 
   if (["Clear"].indexOf(weatherName) !== -1) {
-    resImage.image = weatherImage.sunny;
+    if (cityName == "rome") {
+      resImage.image = weatherImage.italy_day;
+    } else if (cityName == "newyork") {
+      resImage.image = weatherImage.newyork_day;
+    } else if (cityName == "seoul") {
+      resImage.image = weatherImage.korea_day;
+    }
   }
 
   if (["Clouds"].indexOf(weatherName) !== -1) {
     resImage.image = weatherImage.cloudy;
   }
-  
+
   if (["Thunderstorm"].indexOf(weatherName) !== -1) {
     resImage.image = weatherImage.thunderstorms;
   }
